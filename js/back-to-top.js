a39d38abc438224cc0eee4d7548b9909f4cc5a99
Zf0nAYjHjZtO93r <ps2dev@web.de>
2016-08-04 17:57:23 +0200

    fixed settings for menu with css

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
      $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
+            if ($(this).scrollTop() > 100) {
                 $('#back-to-top').fadeIn();
             } else {
                 $('#back-to-top').fadeOut();
